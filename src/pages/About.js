d2ff9172d445d8b99bc431460d673484a1ad26cb
egidehirwa <egidehirwam@gmail.com>
2023-01-17 00:24:13 +0100

    Project setup with the new way of implementinf React Routes since v6.4

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorem
+        ad dolor quidem tempora et eaque labore pariatur dicta, similique
+        veritatis, assumenda reprehenderit aperiam at cupiditate cumque
+        voluptates perferendis! Unde cumque perspiciatis illo nemo suscipit
+        eveniet ratione ducimus voluptatem. Voluptate?
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore impedit
+        id nemo quos dignissimos, culpa ullam aliquam enim, minima velit aperiam
+        voluptatum quaerat dolor sequi?
+      </p>
+    </div>
+  );
+}
